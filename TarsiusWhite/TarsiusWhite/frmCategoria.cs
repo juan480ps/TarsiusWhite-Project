47f6cee9e7eece8c0fbb84880528caf13f74a7b5
FatimaGaona <fatimagaona19@gmail.com>
2019-10-19 22:59:04 -0300

    Diseño del Formulario Categoria

---
+++ TarsiusWhite/TarsiusWhite/frmCategoria.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TarsiusWhite
+{
+    public partial class frmCategoria : Form
+    {
+        public frmCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
