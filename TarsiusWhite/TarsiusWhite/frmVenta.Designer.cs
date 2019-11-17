3ad59ef6c48d8d71f4adeae22622af5c77951ac0
juan480ps <jcorrea480@outlook.com>
2019-11-17 11:20:04 -0300

    cambios venta

---
+++ TarsiusWhite/TarsiusWhite/frmVenta.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace TarsiusWhite
+{
+    partial class frmVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.dtgDetalleVenta = new System.Windows.Forms.DataGridView();
+            this.lblFechaVenta = new System.Windows.Forms.Label();
+            this.dtpFechaVenta = new System.Windows.Forms.DateTimePicker();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.cmbCliente = new System.Windows.Forms.ComboBox();
+            this.lblArticulo = new System.Windows.Forms.Label();
+            this.lblPrecioVenta = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDetalleVenta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(434, 422);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(66, 27);
+            this.btnGuardar.TabIndex = 26;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Detalle";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(195, 181);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 24;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(109, 181);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 23;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(134, 151);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblCategoria.TabIndex = 22;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "Vacuna",
+            "Porcina"});
+            this.cmbCategoria.Location = new System.Drawing.Point(189, 148);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(110, 21);
+            this.cmbCategoria.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Cantidad";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(97, 148);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(24, 20);
+            this.txtCantidad.TabIndex = 18;
+            // 
+            // dtgDetalleVenta
+            // 
+            this.dtgDetalleVenta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDetalleVenta.Location = new System.Drawing.Point(12, 219);
+            this.dtgDetalleVenta.Name = "dtgDetalleVenta";
+            this.dtgDetalleVenta.Size = new System.Drawing.Size(488, 198);
+            this.dtgDetalleVenta.TabIndex = 20;
+            // 
+            // lblFechaVenta
+            // 
+            this.lblFechaVenta.AutoSize = true;
+            this.lblFechaVenta.Location = new System.Drawing.Point(20, 87);
+            this.lblFechaVenta.Name = "lblFechaVenta";
+            this.lblFechaVenta.Size = new System.Drawing.Size(37, 13);
+            this.lblFechaVenta.TabIndex = 19;
+            this.lblFechaVenta.Text = "Fecha";
+            // 
+            // dtpFechaVenta
+            // 
+            this.dtpFechaVenta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaVenta.Location = new System.Drawing.Point(97, 81);
+            this.dtpFechaVenta.Name = "dtpFechaVenta";
+            this.dtpFechaVenta.Size = new System.Drawing.Size(89, 20);
+            this.dtpFechaVenta.TabIndex = 14;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(20, 115);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(39, 13);
+            this.lblCliente.TabIndex = 15;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // cmbCliente
+            // 
+            this.cmbCliente.FormattingEnabled = true;
+            this.cmbCliente.Location = new System.Drawing.Point(96, 111);
+            this.cmbCliente.Name = "cmbCliente";
+            this.cmbCliente.Size = new System.Drawing.Size(203, 21);
+            this.cmbCliente.TabIndex = 16;
+            // 
+            // lblArticulo
+            // 
+            this.lblArticulo.AutoSize = true;
+            this.lblArticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArticulo.Location = new System.Drawing.Point(18, 9);
+            this.lblArticulo.Name = "lblArticulo";
+            this.lblArticulo.Size = new System.Drawing.Size(150, 55);
+            this.lblArticulo.TabIndex = 27;
+            this.lblArticulo.Text = "Venta";
+            // 
+            // lblPrecioVenta
+            // 
+            this.lblPrecioVenta.AutoSize = true;
+            this.lblPrecioVenta.Location = new System.Drawing.Point(321, 155);
+            this.lblPrecioVenta.Name = "lblPrecioVenta";
+            this.lblPrecioVenta.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecioVenta.TabIndex = 29;
+            this.lblPrecioVenta.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(364, 152);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(109, 20);
+            this.txtPrecio.TabIndex = 28;
+            // 
+            // frmVenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(512, 454);
+            this.Controls.Add(this.lblPrecioVenta);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblArticulo);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.dtgDetalleVenta);
+            this.Controls.Add(this.lblFechaVenta);
+            this.Controls.Add(this.dtpFechaVenta);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.cmbCliente);
+            this.Name = "frmVenta";
+            this.Text = "frmVenta";
+            this.Load += new System.EventHandler(this.frmVenta_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDetalleVenta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.DataGridView dtgDetalleVenta;
+        private System.Windows.Forms.Label lblFechaVenta;
+        private System.Windows.Forms.DateTimePicker dtpFechaVenta;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.ComboBox cmbCliente;
+        private System.Windows.Forms.Label lblArticulo;
+        private System.Windows.Forms.Label lblPrecioVenta;
+        private System.Windows.Forms.TextBox txtPrecio;
+    }
+}
---
+++ TarsiusWhite/TarsiusWhite/frmVenta.cs
@@ -0,0 +1,79 @@
+﻿using ClasesTarsius;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TarsiusWhite
+{
+    public partial class frmVenta : Form
+    {
+        Venta venta;
+
+        public frmVenta()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVenta_Load(object sender, EventArgs e)
+        {
+            dtgDetalleVenta.AutoGenerateColumns = true;
+            cmbCategoria.DataSource = Categoria.ObtenerCategoria();
+            cmbCliente.DataSource = Cliente.ObtenerCliente();
+            cmbCliente.SelectedItem = null;
+            cmbCategoria.SelectedItem = null;
+            venta = new Venta();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            DetalleVenta dv = new DetalleVenta();
+            dv.cantidad = Convert.ToInt32(txtCantidad.Text);
+            dv.PrecioVenta = Convert.ToInt32(txtPrecio.Text);
+            dv.categoria = (Categoria)cmbCategoria.SelectedItem;
+            venta.detalleVenta.Add(dv);
+            ActualizarDataGrid();
+
+            Limpiar();
+        }
+
+        private void ActualizarDataGrid()
+        {
+            dtgDetalleVenta.DataSource = null;
+            dtgDetalleVenta.DataSource = venta.detalleVenta;
+        }
+
+        private void Limpiar()
+        {
+            txtCantidad.Text = "0";
+            cmbCategoria.SelectedItem = null;
+            cmbCliente.SelectedItem = null;
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            DetalleVenta dv = (DetalleVenta)dtgDetalleVenta.CurrentRow.DataBoundItem;
+            venta.detalleVenta.Remove(dv);
+            ActualizarDataGrid();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            venta.fechaVenta = dtpFechaVenta.Value.Date;
+            venta.cliente = (Cliente)cmbCliente.SelectedItem;
+
+            Venta.Agregar(venta);
+            MessageBox.Show("El pedido ha sido guardado con éxito");
+            Limpiar();
+            dtgDetalleVenta.DataSource = null;
+            dtpFechaVenta.Value = System.DateTime.Now;
+            cmbCliente.SelectedItem = null;
+            venta = new Venta();
+        }
+    }
+}
