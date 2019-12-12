c317b32940cf5847f254eb15d965f593ca5a2d3b
CatherineGonzalez01 <00dahy@gmail.com>
2019-12-11 21:41:18 -0300

    Codificación de la clase Login

--- TarsiusWhite/ClasesTarsius/Login.cs
+++ TarsiusWhite/ClasesTarsius/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,22 @@ using System.Threading.Tasks;
 namespace ClasesTarsius
 {
     public class Login
+   
     {
+        private const string NombreArchivo = "log.txt";
+        private const string Separador = ";";
+        public static void EscribirLog(string TipoError, string Error)
+        {
+            using (StreamWriter w = File.AppendText(NombreArchivo))
+            {
+                string FechaHora = DateTime.Now.ToShortDateString() + "-" + DateTime.Now.ToLongTimeString();
+                string linea = FechaHora + Separador + TipoError + Separador + Error;
+                w.WriteLine(linea);
+
+            }
+
+
+        }
     }
+}
 }
