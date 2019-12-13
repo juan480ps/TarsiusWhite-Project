b9f91f8fe506310bb40ee174e9b1454764113ef3
CatherineGonzalez01 <00dahy@gmail.com>
2019-12-13 06:11:18 -0300

    Se creo  frm Codigo_de_barras

---
+++ TarsiusWhite/TarsiusWhite/frmCodigo_de_Barras.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TarsiusWhite
+{
+    public partial class frmCodigo_de_Barras : Form
+    {
+        public frmCodigo_de_Barras()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            BarcodeLib.Barcode Codigo = new BarcodeLib.Barcode();
+            Codigo.IncludeLabel = true;
+            panelResultado.BackgroundImage = Codigo.Encode(BarcodeLib.Type.CODE128, txtCodigo.Text, Color.White, 400, 100);
+            btnGuardar.Enabled = true;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Image imgFinal = (Image)panelResultado.BackgroundImage.Clone();
+
+            SaveFileDialog CajaDeDiaologoGuardar = new SaveFileDialog();
+            CajaDeDiaologoGuardar.AddExtension = true;
+            CajaDeDiaologoGuardar.Filter = "Image PNG (*.png)|*.png";
+            CajaDeDiaologoGuardar.ShowDialog();
+            if (!string.IsNullOrEmpty(CajaDeDiaologoGuardar.FileName))
+            {
+                imgFinal.Save(CajaDeDiaologoGuardar.FileName, ImageFormat.Png);
+            }
+            imgFinal.Dispose();
+        }
+    }
+}
