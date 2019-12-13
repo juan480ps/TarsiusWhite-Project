d8757356161f8c6bdbf9781893a37dc9e28f24a9
Vivian Nalerio <vi.nalerio92@gmail.com>
2019-12-12 21:15:23 -0300

    Cambios en formulario Cliente

--- TarsiusWhite/TarsiusWhite/frmCliente.cs
+++ TarsiusWhite/TarsiusWhite/frmCliente.cs
@@ -220,6 +220,8 @@ namespace TarsiusWhite
                 actualizarListadoCliente();
                 cboSexo.DataSource = Enum.GetValues(typeof(Cliente._sexo));
                 cboTipoDocumento.DataSource = Enum.GetValues(typeof(Cliente._tipoDocumento));
+                cboSexo.SelectedItem = null;
+                cboTipoDocumento.SelectedItem = null;
                 bloquearFomulario();
 
             }
